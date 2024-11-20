d0e2dee15b64ee3a1accf4368e41f873878ffef7
MedFediJatlaoui <mohamedfedi.jatlaoui@esprit.tn>
2024-11-20 17:37:51 +0100

    new cv

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Mohamed Fedi Jatlaoui CV.pdf";
+import pdf from "../../Assets/../Assets/CV_jatlaoui_med_fedi.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
